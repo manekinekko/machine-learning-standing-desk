ec500a833991447f424d956d0f4a2a20ca0fdcd7
Wassim Chegham <github@wassim.dev>
2020-10-31 17:54:58 +0100

    chore: initial commit

--- README.md
+++ README.md
@@ -1,27 +1,39 @@
-# StandingDeskMl
+# Controlling a Standing Desk using Machine Learning
+
+![machine-learning-standing-desk-hack-ionion-omega-lobe-ai-angular](src/assets/machine-learning-standing-desk-hack-ionion-omega-lobe-ai-angular.gif)
+
+## Front-end app
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 11.0.0-rc.1.
 
-## Development server
+### Development server
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
 
-## Code scaffolding
+### Code scaffolding
 
 Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
 
-## Build
+### Build
 
 Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory. Use the `--prod` flag for a production build.
 
-## Running unit tests
+### Running unit tests
 
 Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
 
-## Running end-to-end tests
+### Running end-to-end tests
 
 Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protractortest.org/).
 
-## Further help
+### Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+
+## Lobe.ai API server
+
+This demo is using the local API server exposed by [Lobe.ai](https://lobe.ai) ([learn more](https://docs.lobe.ai/docs/export/export/)).
+
+## Modding the standing desk
+
+Read more about the standing desk mod [here](https://dev.to/wassimchegham/hey-google-set-my-desk-to-standingmode-ai6).
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,115 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'standing-desk-ml';
+export class AppComponent implements OnInit {
+  @ViewChild('video', { static: true }) video: ElementRef<HTMLVideoElement>;
+  @ViewChild('canvas', { static: true }) canvas: ElementRef<HTMLCanvasElement>;
+  streaming = false;
+  height = 100;
+  width = 100;
+
+  predictionLabel = 'Unknown';
+  predictionConfidence = '0.00';
+
+  async ngOnInit(): Promise<void> {
+    await this.getMedia();
+
+    setInterval(async (_) => await this.predict(), 1000);
+
+    this.video.nativeElement.addEventListener(
+      'canplay',
+      (ev) => {
+        if (!this.streaming) {
+          this.height =
+            this.video.nativeElement.videoHeight /
+            (this.video.nativeElement.videoWidth / this.width);
+
+          this.video.nativeElement.setAttribute('width', `${this.width}`);
+          this.video.nativeElement.setAttribute('height', `${this.height}`);
+          this.canvas.nativeElement.setAttribute('width', `${this.width}`);
+          this.canvas.nativeElement.setAttribute('height', `${this.height}`);
+          this.streaming = true;
+        }
+      },
+      false
+    );
+  }
+
+  async predict(): Promise<void> {
+    const data = this.snapshot();
+    const response = await fetch(
+      'http://localhost:38100/predict/2981ee27-3ecd-4c4e-b0b7-abd2960fcf5f',
+      {
+        method: 'post',
+        body: JSON.stringify({
+          inputs: {
+            Image: data,
+          },
+        }),
+      }
+    );
+
+    const json = await response.json();
+    const pred = json.outputs?.Prediction?.pop();
+    const predConfidence = json.outputs.Labels.filter(el => el[0] === pred).pop();
+
+    if (pred !== this.predictionLabel) {
+      this.predictionLabel = pred;
+      this.predictionConfidence = Number(predConfidence[1]).toFixed(2);
+      // await this.setDeskPosition(this.predictionLabel);
+    }
+  }
+
+  async setDeskPosition(position: string): Promise<void> {
+    const modes = {
+      Standing: 2,
+      Sitting: 1,
+    };
+
+    if (modes[position]) {
+      const res = await fetch(
+        `http://192.168.86.37:1337/mode/${modes[position]}`
+      );
+    }
+  }
+
+  snapshot(): string {
+    const context = this.canvas.nativeElement.getContext('2d');
+    if (this.width && this.height) {
+      this.canvas.nativeElement.width = this.width;
+      this.canvas.nativeElement.height = this.height;
+      context.drawImage(
+        this.video.nativeElement,
+        0,
+        0,
+        this.width,
+        this.height
+      );
+
+      return this.canvas.nativeElement
+        .toDataURL()
+        .replace('data:image/png;base64,', '');
+    } else {
+      return null;
+    }
+  }
+
+  async getMedia(): Promise<void> {
+    let stream = null;
+
+    try {
+      stream = await navigator.mediaDevices.getUserMedia({
+        audio: true,
+        video: true,
+      });
+      this.video.nativeElement.srcObject = stream;
+      this.video.nativeElement.play();
+    } catch (err) {
+      console.error(err);
+    }
+  }
 }
